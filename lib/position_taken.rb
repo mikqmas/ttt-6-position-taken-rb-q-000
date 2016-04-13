4bab139cabdc57fd29eeca809ad6966b6bf2b5ef
mikqmas <samkim86@gmail.com>
2016-04-12 23:13:04 -0700

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, pos)
+  empty = [" ", "", nil]
+  p board
+  empty.none?{|x| x ==  board[pos]}
+end
+
+p position_taken?(["","","X"], 3)
+
